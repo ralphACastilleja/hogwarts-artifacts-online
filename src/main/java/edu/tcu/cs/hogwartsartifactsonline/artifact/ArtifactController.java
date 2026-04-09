7e330258d7f80cfd1660d2370eabd5e7948cd822
RalphCastilleja <r.a.castilleja@tcu.edu>
2026-04-09 11:49:52 -0500

    FULL code of vids 42-52

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/artifact/ArtifactController.java
@@ -0,0 +1,76 @@
+package edu.tcu.cs.hogwartsartifactsonline.artifact;
+
+import edu.tcu.cs.hogwartsartifactsonline.artifact.converter.ArtifactDtoToArtifactConverter;
+import edu.tcu.cs.hogwartsartifactsonline.artifact.converter.ArtifactToArtifactDtoConverter;
+import edu.tcu.cs.hogwartsartifactsonline.artifact.dto.ArtifactDto;
+import edu.tcu.cs.hogwartsartifactsonline.system.Result;
+import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("${api.endpoint.base-url}/artifacts")
+public class ArtifactController {
+
+    private final ArtifactService artifactService;
+
+    private final ArtifactToArtifactDtoConverter artifactToArtifactDtoConverter;
+
+    private final ArtifactDtoToArtifactConverter artifactDtoToArtifactConverter;
+
+    private final MeterRegistry meterRegistry;
+
+
+    public ArtifactController(ArtifactService artifactService, ArtifactToArtifactDtoConverter artifactToArtifactDtoConverter, ArtifactDtoToArtifactConverter artifactDtoToArtifactConverter, MeterRegistry meterRegistry) {
+        this.artifactService = artifactService;
+        this.artifactToArtifactDtoConverter = artifactToArtifactDtoConverter;
+        this.artifactDtoToArtifactConverter = artifactDtoToArtifactConverter;
+        this.meterRegistry = meterRegistry;
+    }
+
+    @GetMapping("/{artifactId}")
+    public Result findArtifactById(@PathVariable String artifactId){
+        Artifact foundArtifact = this.artifactService.findById(artifactId);
+        this.meterRegistry.counter("artifact.id." + artifactId).increment();
+        ArtifactDto artifactDto = this.artifactToArtifactDtoConverter.convert(foundArtifact);
+        return new Result(true, StatusCode.SUCCESS, "Find One Success", artifactDto);
+    }
+
+    @GetMapping
+    public Result findAllArtifacts(){
+        List<Artifact> foundArtifacts = this.artifactService.findAll();
+        // Convert foundArtifacts to a list of artifactDtos
+        List<ArtifactDto> artifactDtos = foundArtifacts.stream()
+                .map(this.artifactToArtifactDtoConverter::convert)
+                .collect(Collectors.toList());
+        return new Result(true, StatusCode.SUCCESS, "Find All Success", artifactDtos);
+    }
+
+    @PostMapping
+    public Result addArtifact(@Valid @RequestBody ArtifactDto artifactDto){
+        // Convert artifactDto to artifact
+        Artifact newArtifact = this.artifactDtoToArtifactConverter.convert(artifactDto);
+        Artifact savedArtifact = this.artifactService.save(newArtifact);
+        ArtifactDto savedArtifactDto = this.artifactToArtifactDtoConverter.convert(savedArtifact);
+        return new Result(true, StatusCode.SUCCESS, "Add Success", savedArtifactDto) ;
+    }
+
+    @PutMapping("/{artifactId}")
+    public Result updateArtifact(@PathVariable String artifactId, @Valid @RequestBody ArtifactDto artifactDto){
+        Artifact update = this.artifactDtoToArtifactConverter.convert(artifactDto);
+        Artifact updatedArtifact = this.artifactService.update(artifactId, update);
+        ArtifactDto updatedArtifactDto = this.artifactToArtifactDtoConverter.convert(updatedArtifact);
+        return new Result(true, StatusCode.SUCCESS, "Update Success", updatedArtifactDto);
+    }
+
+    @DeleteMapping("/{artifactId}")
+    public Result deleteArtifact(@PathVariable String artifactId){
+        this.artifactService.delete(artifactId);
+        return new Result(true, StatusCode.SUCCESS, "Delete Success");
+    }
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,15 @@
+package edu.tcu.cs.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(value = "dev")
+class HogwartsArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
